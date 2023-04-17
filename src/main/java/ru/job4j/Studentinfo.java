8e7b70ed14d943cbb135cb75ede152305146f132
загрузка файлов в github <m_rail@mail.ru>
2023-04-17 18:32:55 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Rail Musin");
+        System.out.println("23.11.1990");
+    }
+}
